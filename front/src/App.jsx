67c336700fde87730597b3ddd612364578e78b51
Pedro Santana <pedro.ana@nexti.com>
2024-11-10 15:50:58 -0300

    add react-electron project

---
+++ front/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="node-version"></span>
+      <span className="chrome-version"></span>
+      <span className="electron-version"></span>
+    </div>
+  );
+}
+
+export default App;
