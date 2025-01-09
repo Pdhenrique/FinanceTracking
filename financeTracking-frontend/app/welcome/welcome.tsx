8e8e683fdc7ba7a8241ba26f10dde8fda46fc37e
Pdhenrique <pedroschw.c@gmail.com>
2025-01-09 19:45:19 -0300

    feat: added shadcnUi

--- financeTracking-frontend/app/welcome/welcome.tsx
+++ financeTracking-frontend/app/welcome/welcome.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
 export function Welcome() {
   return (
-    <></>
+    <>
+      <div>TESTANDO</div>
+      <Button>
+        Teste
+      </Button>
+    </>
   );
 }
