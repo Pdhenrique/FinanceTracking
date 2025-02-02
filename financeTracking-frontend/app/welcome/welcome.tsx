e4c98484582a8c981ee23107685b04c745f6538c
Pedro Santana <pedro.ana@nexti.com>
2025-02-02 11:31:16 -0300

    removed shadcn components

--- financeTracking-frontend/app/welcome/welcome.tsx
+++ financeTracking-frontend/app/welcome/welcome.tsx
@@ -1,14 +1,5 @@
-import { Button } from "@/components/ui/button";
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
-
 export function Welcome() {
   return (
-    <>
-      <div>TESTANDO</div>
-      <Button>
-        Teste
-      </Button>
-    </>
+   <></>
   );
 }
