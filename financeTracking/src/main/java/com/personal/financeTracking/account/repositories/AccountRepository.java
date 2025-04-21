196c9add881d4e11afb745aac927569998e70c02
Pdhenrique <pedroschw.c@gmail.com>
2025-04-21 18:17:21 -0300

    added transaction type and name, and added account in transaction entity

--- financeTracking/src/main/java/com/personal/financeTracking/account/repositories/AccountRepository.java
+++ financeTracking/src/main/java/com/personal/financeTracking/account/repositories/AccountRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
-    Optional<Account> findByUserId(String userId);
+    Optional<Account> findByUserId(UUID userId);
 }
